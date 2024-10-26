51ea7eea2116dccf8fbf73558d85855f30901570
anton <anton@gmail.com>
2024-10-26 12:24:18 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -13,3 +15,42 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    paginate_by = 5
+
+    def get_queryset(self):
+        return Manufacturer.objects.all().order_by("name")
+
+
+class CarListView(generic.ListView):
+    model = Car
+    context_object_name = "car_list"
+    paginate_by = 5
+
+    def get_queryset(self):
+        return Car.objects.select_related("manufacturer")
+
+
+def car_detail_view(request: HttpRequest, pk: int) -> HttpResponse:
+    car = Car.objects.get(pk=pk)
+    context = {
+        "car": car,
+    }
+    return render(request, "taxi/car_detail.html", context=context)
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    template_name = "taxi/driver_detail.html"
+    context_object_name = "driver"
+
+    def get_queryset(self):
+        return Driver.objects.prefetch_related("cars__manufacturer").all()
